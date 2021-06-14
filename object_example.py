7f92cf56015d55087beb8b48789ef90682864870
OctavioOp <oc.opazoutlook.com>
2021-06-14 19:15:14 -0400

    reordenamiento

--- object_example.py
+++ object_example.py
@@ -9,6 +9,7 @@ class User:
         return self
     def make_withdrawal(self, amount):
         self.account_balance = self.account_balance - amount
+        return self
     def display_user_balance(self):
         mount = str(self.account_balance)
         return print(f"el/la señor/ita {self.name}, posee en su cuenta: $ {mount}")
@@ -16,19 +17,21 @@ class User:
         if(other_user != None):
             self.account_balance = self.account_balance-amount
             other_user.make_deposit(amount)
+            return self
         else:
             return print('Ingrese Usuario Valido')
         
         
-
+#crear usuario/////////////////////////////////////////
 carlitos = User('Carlitos Lechuga', 'carlechu@asd.asd')
-print(carlitos.name, carlitos.email)
-carlitos.make_deposit(5000).display_user_balance()
 juanin = User('Juanin Juan Harry', 'jujuha.asd@asd')
-print(juanin.name, juanin.email)
+#print(carlitos.name, carlitos.email)
+#print(juanin.name, juanin.email)
+#movimientos///////////////////////////////////////////
+carlitos.make_deposit(5000).display_user_balance()
 juanin.make_deposit(100).display_user_balance()
-carlitos.transfer_money(juanin, 2000)
 print('--momento--transferencia--')
+carlitos.transfer_money(juanin, 2000)
 juanin.display_user_balance()
 carlitos.display_user_balance()
 
